752a53c826864ecabcd001852e6481402e3bea1e
Patrick Mason <patrickmason73@gmail.com>
2023-01-29 21:35:31 -0700

    complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,16 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  // const name = projects.name
+  // const about = projects.about
+  // const technologies = projects.technologies
+  // const id = projects.id
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map(({ name, about, technologies }, id) => {
+        return <ProjectItem key={id} name={name} about={about} technologies={technologies} />
+      })}</div>
     </div>
   );
 }
